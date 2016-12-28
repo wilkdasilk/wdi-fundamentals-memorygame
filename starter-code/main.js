7ac0fcc579716a89ba111577db01b9c31bba74d5
wilkdasilk <anwilk92@gmail.com>
2016-12-28 13:39:32 -0800

    alert message for card comparison

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+
+if (cardTwo === cardFour) {
+    alert("You found a match!")
+}
+else alert("Sorry, try again.");
+
