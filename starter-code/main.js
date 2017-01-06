d589e30f54655ab5fe1e1ab5b9baac94c9bfad94
wilkdasilk <anwilk92@gmail.com>
2017-01-06 14:26:51 -0800

    4 cards made using JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,18 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
     alert("You found a match!")
 }
-else alert("Sorry, try again.");
+else alert("Sorry, try again.");*/
 
+
+var createCards = function() {
+    var gameBoard = document.getElementById('game-board');
+    for (i=0; i<4; i++) {
+        var newDiv = document.createElement('div');
+        newDiv.className ='card';
+        gameBoard.appendChild(newDiv)
+    }
+};
+createCards();
