b4e4f2723f974a451c56f6bc8ff5495a039c2f5e
wilkdasilk <anwilk92@gmail.com>
2017-01-09 16:04:39 -0800

    clickable functions and images

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,9 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
-var cardOne = 'queen';
+/*var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
-var cardFour = 'king';
+var cardFour = 'king';*/
 
 /*if (cardTwo === cardFour) {
     alert("You found a match!")
@@ -11,12 +11,80 @@ var cardFour = 'king';
 else alert("Sorry, try again.");*/
 
 
-var createCards = function() {
+/*var createCards = function() {
     var gameBoard = document.getElementById('game-board');
     for (i=0; i<4; i++) {
-        var newDiv = document.createElement('div');
-        newDiv.className ='card';
-        gameBoard.appendChild(newDiv)
+        var cardElement = document.createElement('div');
+        cardElement.className ='card';
+        gameBoard.appendChild(cardElement);
     }
 };
-createCards();
+createCards();*/
+
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardElements = [];
+
+var createBoard = function(){
+    
+    //latches onto HTML
+     var gameBoard = document.getElementById('game-board');
+    
+    //creates each card and sets it up
+     for (var i=0; i<cards.length; i++) {
+        
+         var cardElement = document.createElement('div');
+         cardElement.className ='card';
+         gameBoard.appendChild(cardElement);
+         cardElements[i] = cardElement;
+         cardElements[i].setAttribute('data-card', cards[i]);
+         cardElements[i].addEventListener('click', addImg);
+         cardElements[i].addEventListener('click', isTwoCards);
+                                    
+         //defining function, add clicked card to cardsInPlay and triggers our test if we win
+         var isTwoCards = function() {
+            cardsInPlay.push(this.getAttribute('data-card'));
+            if (cardsInPlay.length === 2) {
+                isMatch(cardsInPlay);
+                cardsInPlay = [];
+                                }
+         }; 
+         
+         //defining function, adds image to clicked card
+         var addImg = function() {
+            if (this.getAttribute('data-card') == "king") {
+                this.innerHTML = "<img src='king.png' alt='King' />"}
+            else {
+                this.innerHTML = "<img src='queen.png' alt='queen' />"}
+         };
+         
+         //defining function, tests our cards, gives result and clears board
+        var clearBoard = function(){
+          for (var i=0; i<cards.length; i++) {
+                   cardElements[i].innerHTML = ""
+                 }  
+        };
+         
+         var isMatch = function(x) {
+             if (x[0] == x[1]) {
+                 console.log("It's a match!");
+              }
+             else {
+                  console.log("Sorry, not a match");
+             }
+             setTimeout(clearBoard(), 2000); 
+          };
+        };
+};
+
+//runs everything
+createBoard();
+
+
+//time delay on clearBoard isn't working 
+
+//using the inspector we know card one is a queen, however it doesn't respond to being clicked
+//when you offset the cards so there are only three, the first card still can't be clicked
